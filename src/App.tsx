9ad22d87ccdda6bd0ba138b4e4dc05856e7c2d3e
fireonex <mari.krulikovskaya@mail.ru>
2024-04-18 18:14:39 +0300

    first commit (first version of form with validation)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {ChangeEvent, useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const inputNameHandler = (e: ChangeEvent<HTMLInputElement> ) => {
+        setName(e.currentTarget.value.trim())
+    }
+    const inputEmailHandler = (e: ChangeEvent<HTMLInputElement> ) => {
+        setEmail(e.currentTarget.value.trim())
+    }
+    const inputPasswordHandler = (e: ChangeEvent<HTMLInputElement> ) => {
+        setPassword(e.currentTarget.value.trim())
+    }
+
+    const validatePassword = (password: string) => {
+        return password.length >= 6 && /[A-Z]/.test(password) && /\d/.test(password);
+    }
+
+    const validateEmail = (email: string) => {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    }
+
+    const buttonDisabledHandler = !(name && validateEmail(email) && validatePassword(password))
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Заполните форму</h2>
+        <div className={'inputsWrapper'}>
+            <div>
+                <input type={"text"} placeholder={'имя'} onChange={inputNameHandler} value={name}/>
+                {name.trim() !== '' ? <span>Спасибо!</span> : <span>Введите корректное имя</span>}
+            </div>
+            <div>
+                <input type={"email"} placeholder={'email'} onChange={inputEmailHandler} value={email}/>
+                {validateEmail(email) ? <span>Спасибо!</span> : <span>Введите корректный email</span>}
+            </div>
+            <div>
+                <input type={"password"} placeholder={'пароль'} onChange={inputPasswordHandler} value={password}/>
+                {validatePassword(password) ? <span>Спасибо!</span> : <span>Введите корректный пароль</span>}
+            </div>
+            <button disabled={buttonDisabledHandler}>send</button>
+        </div>
     </div>
   );
 }
